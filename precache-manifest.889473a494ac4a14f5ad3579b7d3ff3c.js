b8428f2390248fd67437d9d658ad1299a5514192
SkSumit <kolsum24@gmail.com>
2020-08-24 17:27:07 +0530

    Updates

---
+++ precache-manifest.889473a494ac4a14f5ad3579b7d3ff3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cee1328380a63be52d2f982f1859c5b",
+    "url": "/Clubs/index.html"
+  },
+  {
+    "revision": "ff2fcfe16d6457286d59",
+    "url": "/Clubs/static/js/2.a2ec5fc2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Clubs/static/js/2.a2ec5fc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da1adbfb0cd20753a735",
+    "url": "/Clubs/static/js/main.2c3e88f0.chunk.js"
+  },
+  {
+    "revision": "7e1c641912f1b12148c4",
+    "url": "/Clubs/static/js/runtime-main.72eb142b.js"
+  },
+  {
+    "revision": "51675f87370f0c86e971f4b1cc779f24",
+    "url": "/Clubs/static/media/flag-orpheus-top.51675f87.svg"
+  },
+  {
+    "revision": "946ba1e8ee14eb587eb39aa0b9f0326a",
+    "url": "/Clubs/static/media/skn.946ba1e8.png"
+  }
+]);
